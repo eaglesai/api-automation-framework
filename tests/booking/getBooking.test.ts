5e8ef65b7c6239a0097ec6417a5330213b29f20f
Sai <time2learnai@gmail.com>
2026-04-20 13:08:46 -0400

    GET Booking API coded and tested

--- tests/booking/getBooking.test.ts
+++ tests/booking/getBooking.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-//import { request } from 'node:http';
+import { BookingID, Booking } from '../../types/booking.types';
 
 test('GET API Call - Fetch all Booking records',async({request})=>{
 
@@ -10,4 +10,27 @@ test('GET API Call - Fetch all Booking records',async({request})=>{
     expect(resBody.length).toBeGreaterThan(0);
     console.log('First Booking:  ', JSON.stringify(resBody[0], null, 2));
     console.log('Last Booking:   ', JSON.stringify(resBody[resBody.length-1],null,2));
+});
+
+test('GET DETAILS ABOUT THE BOOKING REQUEST ', async({request}) =>{
+    const response = await request.get('/booking');
+    expect(response.status()).toBe(200);
+    const respBody : BookingID[] = await response.json();
+    // I would like to have a for loop or while loop -- STARTING FROM HERE
+    const respBookingID : number = respBody[0].bookingid;
+
+    const respBookingSingleID = await request.get(`/booking/${respBookingID}`);
+    expect(respBookingSingleID.status()).toBe(200);
+
+    const respBodyDetail : Booking = await respBookingSingleID.json();
+        expect(respBodyDetail).toHaveProperty('firstname');
+        expect(respBodyDetail).toHaveProperty('lastname');
+        expect(respBodyDetail).toHaveProperty('totalprice');
+        expect(respBodyDetail).toHaveProperty('depositpaid');
+        expect(respBodyDetail.bookingdates).toHaveProperty('checkin');
+        expect(respBodyDetail.bookingdates).toHaveProperty('checkout');
+    
+    console.log('Booking Details  ', JSON.stringify(respBodyDetail,null,2));
+    // I would like to have a for loop or while loop -- END FROM HERE  
+
 });
---
+++ types/booking.types.ts
@@ -0,0 +1,46 @@
+
+export interface BookingID{
+    bookingid : number;
+}
+
+export interface BookingDates {
+  checkin: string;
+  checkout: string;
+}
+
+export interface Booking {
+  firstname: string;
+  lastname: string;
+  totalprice: number;
+  depositpaid: boolean;
+  bookingdates: BookingDates; //it is an object so this need to be defined separately.
+  additionalneeds?: string;  // ← ? means optional
+}
+
+/*
+Success 200
+Field	Type	Description
+firstname	String	
+Firstname for the guest who made the booking
+
+lastname	String	
+Lastname for the guest who made the booking
+
+totalprice	Number	
+The total price for the booking
+
+depositpaid	Boolean	
+Whether the deposit has been paid or not
+
+bookingdates	Object	
+Sub-object that contains the checkin and checkout dates
+
+  checkin	Date	
+Date the guest is checking in
+
+  checkout	Date	
+Date the guest is checking out
+
+additionalneeds	String	
+Any other needs the guest has
+*/
